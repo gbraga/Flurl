a29e7f13beb4b1b6b3672f0a49efb98393fa2ad1
Gabriel Braga <gbraga.dev@gmail.com>
2019-03-04 16:25:07 -0300

    startup project

---
+++ FlurTest.Test/FakeJsonServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Flurl.Http.Testing;
+using FlurlTest;
+using FlurlTest.Tests;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class FakeJsonServiceTests
+    {
+        private FakeJsonService _fakeJsonService;
+
+        [Test]
+        public async Task GetFakeDataShouldCallFakeJsonServerWithCorrectParameters()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                _fakeJsonService = new FakeJsonService(new HttpClient(new FakeHttpClientMessageHandler()));
+
+                var templateDados = @"{
+                                  ""token"": ""4UByP0KUbLPL_KMIZNWRbg"",
+                                  ""data"": {
+                                    ""id"": ""personNickname"",
+                                    ""email"": ""internetEmail"",
+                                    ""last_login"": {
+                                      ""date_time"": ""dateTime|UNIX"",
+                                      ""ip4"": ""internetIP4""
+                                    }
+                                  }
+                                }";
+
+                var response = await _fakeJsonService.GetFakeData();
+
+                httpTest.ShouldHaveCalled("https://app.fakejson.com/q")
+                         .WithVerb(HttpMethod.Post)
+                         .WithContentType("application/json")
+                         .WithRequestBody(templateDados)
+                         .Times(1);
+            }
+        }
+    }
+}
---
+++ FlurlTest/FakeJsonService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlurlTest
+{
+    public class FakeJsonService
+    {
+        private readonly HttpClient _httpClient;
+
+        public FakeJsonService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetFakeData()
+        {
+            var templateDados = @"{
+                                  ""token"": ""4UByP0KUbLPL_KMIZNWRbg"",
+                                  ""data"": {
+                                    ""id"": ""personNickname"",
+                                    ""email"": ""internetEmail"",
+                                    ""last_login"": {
+                                      ""date_time"": ""dateTime|UNIX"",
+                                      ""ip4"": ""internetIP4""
+                                    }
+                                  }
+                                }";
+
+            var response = await _httpClient.PostAsync(
+                                                    "https://app.fakejson.com/q",
+                                                    new StringContent(
+                                                        templateDados, 
+                                                        Encoding.UTF8, 
+                                                        "application/json"
+                                                    )
+                                                );
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
